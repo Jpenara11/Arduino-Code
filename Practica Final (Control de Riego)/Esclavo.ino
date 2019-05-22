3ab97babc05b1f57971fef36ac2b7fcb8d6510a8
Jaime de la Peña <jpenara11@usal.es>
2019-05-22 21:22:51 +0200

    FIN

---
+++ Practica Final (Control de Riego)/Esclavo.ino
@@ -0,0 +1,320 @@
+/* CONTROL DE RIEGO: PARTE 2 (ARDUINO ESCLAVO)
+   AUTORES: De la Peña Ramos, Jaime
+            Martin Garcia, Juan Carlos
+            Montero Fernandez, Alberto
+   FECHA DE LA DEFENSA: 22/05/19
+
+            UNIVERSIDAD DE SALAMANCA 2018-2019
+*/
+            
+#include <EasyTransfer.h>
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+#include <RTClib.h>
+#include <Key.h>
+#include <Keypad.h>
+
+#define FIL 2
+#define COL 16
+#define POS_LCD 0x3F
+
+#define FIL_TECLADO 4
+#define COL_TECLADO 4
+
+#define TAM_CONTRASENA 5
+
+EasyTransfer ETout;
+
+struct RECEIVE_DATA_STRUCTURE
+{
+  int higrometro;
+  float humedad;
+  float temperatura;
+  char fechaYhora[12];
+  char ubicacion[23];
+  char estadoTallo[9];
+};
+
+struct RECEIVE_DATA_STRUCTURE recibido;
+
+LiquidCrystal_I2C lcd(POS_LCD, COL, FIL);  // Inicia el LCD con los datos introducidos
+
+char teclas[FIL_TECLADO][COL_TECLADO]
+{
+  { '1','2','3', 'A' },
+  { '4','5','6', 'B' },
+  { '7','8','9', 'C' },
+  { '*','0','#', 'D' }
+};
+
+const byte filasPins[FIL_TECLADO] = { 9, 8, 7, 6 };
+const byte columnasPins[COL_TECLADO] = { 5, 4, 3, 2 };
+Keypad tecladoMembrana = Keypad(makeKeymap(teclas), filasPins, columnasPins, FIL_TECLADO, COL_TECLADO);
+char teclaPulsada;
+char contrasena[TAM_CONTRASENA] = "3515";
+char claveUsuario[TAM_CONTRASENA];
+int indice = 0;
+int alarma = 1;
+
+const int Buzzer = 11;
+
+const int PIR = 13; // Pin de entrada para el sensor PIR (Passive Infrared Sensor)
+int estadoPIR = -1; // 4 Estados (-1, 0 ,1 y 2)
+int valorPIR = LOW;  // De inicio el PIR está apagado
+
+const int SensorLlama = 10; // Pin de entrada para el sensor de Llama Infrarrojo
+int valorSensorLlama = 1; // Estado sensor Llama
+
+const int Led = 12;
+
+int higrometroLCD;
+float temperaturaLCD, humedadLCD;
+String ubicacionLCD, estadoTalloLCD, fechaLCD;
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.init();                      
+  lcd.backlight();
+
+  pinMode(Buzzer, OUTPUT);
+  pinMode(PIR, INPUT_PULLUP);
+  pinMode(SensorLlama, INPUT_PULLUP);
+  pinMode(Led, OUTPUT);
+
+  ETout.begin(details(recibido), &Serial);
+}
+
+void loop() 
+{
+  valorPIR = digitalRead(PIR);
+
+  valorSensorLlama = digitalRead(SensorLlama);
+  
+   if (valorSensorLlama == LOW)
+   {
+      estadoPIR= 2;
+   }
+  
+  switch(estadoPIR)
+  {
+    case -1: // Carga condensadores pre-inicio
+            delay(2000); // Esperamos 20 segundos a que se carguen los condensadores
+            estadoPIR = 0;
+            Serial.println("ESTADO CARGA CONDENSADORES");
+            break;
+            
+    case 0: // Alarma apagada, esperando a que el usuario la inicie
+            Serial.println("ESTADO ALARMA APAGADA");
+            detectarTecla();
+            break;
+            
+     case 1:
+            Serial.println("ESTAMOS EN EL ESTADO 1");
+            if (valorPIR == HIGH) estadoPIR = 2; 
+            else estadoPIR = 1;
+            break;
+            
+     case 2:  
+              saltarAlarmaLCD();
+              alarma = 1;
+              activarAlarma(alarma);
+              
+              estadoPIR = 0;
+              digitalWrite(Led, LOW);
+              digitalWrite(Buzzer, LOW);
+              delay(5000); //Alarma desactivada
+              desactivarAlarmaLCD();
+              break; 
+  }
+
+ if(ETout.receiveData())
+ {
+  Serial.println(recibido.higrometro);
+  Serial.println(recibido.humedad);
+  Serial.println(recibido.temperatura);
+  Serial.println(recibido.fechaYhora);
+  Serial.println(recibido.ubicacion);
+  Serial.println(recibido.estadoTallo);
+
+  String fechaYhoraString(recibido.fechaYhora);
+  String ubicacionString(recibido.ubicacion);
+  String estadoTalloString(recibido.estadoTallo);
+
+  higrometroLCD = recibido.higrometro;
+  temperaturaLCD = recibido.temperatura;
+  humedadLCD = recibido.humedad;
+  fechaLCD = fechaYhoraString;
+  ubicacionLCD = ubicacionString;
+  estadoTalloLCD = estadoTalloString;
+ }
+
+ if(estadoPIR == 0)
+ {
+    detectarTecla();
+    imprimirLCDTempyHum(temperaturaLCD,humedadLCD);
+    delay(5000);
+  
+    detectarTecla();
+    imprimirLocalizacionFecha("N 44.4 E 55.1",fechaLCD);
+    delay(5000);
+
+    detectarTecla();
+    imprimirHumedadTierraYTallo(higrometroLCD,estadoTalloLCD);
+    delay(5000);
+
+    detectarTecla();
+
+    if(estadoPIR == 1)
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("ALARMA CONECTADA");
+    }
+ }
+
+}
+
+void detectarTecla()
+{
+  teclaPulsada = tecladoMembrana.getKey();
+  
+  if (teclaPulsada == '*')
+  {
+     Serial.println(teclaPulsada);
+     estadoPIR = 1;
+     avisarAlarmaLCD();     
+  }
+}
+
+void activarAlarma(int alarma)
+{
+  digitalWrite(Led, HIGH);
+  digitalWrite(Buzzer, HIGH);
+  Serial.println("ALARMA!!");
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("CLAVE: ");
+  lcd.setCursor(7,0);
+  while(alarma == 1)
+  {
+    teclaPulsada = tecladoMembrana.getKey();
+    if(teclaPulsada)
+    {
+      claveUsuario[indice] = teclaPulsada;
+      indice++;
+      lcd.print(teclaPulsada);
+    }
+  
+    if(indice == TAM_CONTRASENA - 1)
+    {
+      indice = 0;
+      if(strcmp(claveUsuario, contrasena) == 0)
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("CONTRASENA: ");
+        lcd.setCursor(0,1);
+        lcd.print("CORRECTA");
+        analogWrite(Buzzer, LOW);
+        alarma = 0;
+      }
+      else
+      {
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("CONTRASENA: ");
+        lcd.setCursor(0,1);
+        lcd.print("INCORRECTA");
+        delay(3000);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("CLAVE: ");
+        lcd.setCursor(7,0);
+      }
+    }
+  }
+}
+
+void avisarAlarmaLCD()
+{
+  int contador = 10;
+  
+  lcd.clear();
+
+  while(contador > 0)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("ALARMA INICIADA");
+    lcd.setCursor(8,1);
+    lcd.print(contador);
+    delay(1000);
+    contador--;
+    lcd.clear();
+  }
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("ALARMA CONECTADA");
+  delay(3000);
+}
+
+void saltarAlarmaLCD()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("ALARMA ACTIVADA!");
+}
+
+void desactivarAlarmaLCD()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("ALARMA APAGADA");
+  delay(3000);
+  lcd.clear();
+}
+
+void imprimirLCDTempyHum(float temperatura, float humedad)
+{
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print("TEMP: ");
+   lcd.print(temperatura);
+   lcd.print(" gC");
+   lcd.setCursor(0,1);
+   lcd.print("HUMD: ");
+   lcd.print(humedad);
+   lcd.print(" %");
+}
+
+void imprimirLocalizacionFecha(String localizacion, String fecha)
+{
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print("L: ");
+   lcd.print(localizacion);
+   lcd.setCursor(0,1);
+   lcd.print("HYF: ");
+   lcd.print(fecha);
+}
+
+void imprimirHumedadTierraYTallo(int higrometro, String estadoTallo)
+{
+   lcd.clear();
+   lcd.setCursor(0,0);
+   lcd.print("HUMD TIERR: ");
+   lcd.print(higrometro);
+   lcd.print(" %");
+   lcd.setCursor(0,1);
+   lcd.print("TALLO: ");
+   lcd.print(estadoTallo);
+}
+
+void imprimirFecha(DateTime fecha)
+{
+  Serial.print(fecha.day(), DEC);
+  Serial.print('/');
+  Serial.print(fecha.month(), DEC);
+  Serial.print('/');
+  Serial.print(fecha.year(), DEC);
+}
---
+++ Practica Final (Control de Riego)/Maestro.ino
@@ -0,0 +1,263 @@
+/* CONTROL DE RIEGO: PARTE 2 (ARDUINO MAESTRO)
+   AUTORES: De la Peña Ramos, Jaime
+            Martin Garcia, Juan Carlos
+            Montero Fernandez, Alberto
+   FECHA DE LA DEFENSA: 22/05/19
+
+            UNIVERSIDAD DE SALAMANCA 2018-2019
+*/
+
+#include <EasyTransfer.h>
+#include <Wire.h>
+#include <DHT.h>
+#include <RTClib.h>
+#include <Servo.h>
+#include <SoftwareSerial.h>
+
+EasyTransfer ETin;
+
+const int HigrometroPin = A0; // Declarar pin A0 Higrometro
+int valorHigrometro = 0; // Variable donde se almacena el valor leido por el Higrometro
+int valorHigrometroMap = 0; // Variable donde almacena el valor mapeado del Higrometro
+
+const int FlexometroPin = A1; // Declarar pin A1 Flexometro
+int valorFlexometro = 0; // Variable donde se almacena el valor leido por el Flexometro
+int valorFlexometroMap = 0; // Variable donde almacena el valor mapeado del Flexometro
+String estadoTallo; // Variable donde se almacena el estado del tallo tiene 3 (Recto, Torcido, +Torcido)
+char arrayEstadoTallo[9];
+
+const int SensorTempYHum = 2; // Declarar pin 2 sensor de humedad y temperatura
+float valorHumedad = 0.0; // Variable donde se almacena el valor de la humedad leido por el DHT11
+float valorTemperatura = 0.0; // Variable donde se almacena el valor de la temperatura leido por el DHT11
+DHT dht(SensorTempYHum, DHT11); // Inicializar el sensor DHT11 (Temperatura y Humedad)
+
+const int GpsRx = 10; // Declarar pin 4 GPS pin RX
+const int GpsTx = 11; // Declarar pin 3 GPS pin TX
+String localizacion; // Declarar variable donde se almacena la localización leída por el GPS
+char arrayLocalizacion[30];
+SoftwareSerial gpsSerial(GpsRx, GpsTx); // Crear objeto GPS
+const int sentenceSize = 80;
+char sentence[sentenceSize];
+
+const int EntradaServo = 6; // Declarar pin 6 servo
+Servo servo; // Declarar objeto servo
+int cantidadRiego = 5; // Número de veces que el servo se desplaza para regar
+
+
+RTC_DS3231 rtc3231; // Inicializar RTC DS3231 (Reloj y Calendario)
+String fechaYhoraActual;
+char arrayHoraYFecha[12];
+
+struct SEND_DATA_STRUCTURE
+{
+  int higrometro;
+  float humedad;
+  float temperatura;
+  char fechaYhora[12];
+  char ubicacion[23];
+  char estadoTallo[9];
+};
+
+struct SEND_DATA_STRUCTURE informacion;
+
+String horaYFechaActualString (DateTime fechaActual);
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  dht.begin(); // Se inicia el sensor de temperatura y humedad 
+   
+  if (! rtc3231.begin())// En caso de que no se consiga iniciar el reloj
+  {
+    Serial.println("No se ha podido detectar el RTC_DS3231"); // Se notifica por puerto serie
+    while (1); // Bucle infinito para no continuar el programa
+  }
+  
+  rtc3231.adjust(DateTime(F(__DATE__), F(__TIME__))); // Ajustar fecha y hora del reloj
+
+  gpsSerial.begin(9600); // Inicializar GPS
+
+  pinMode(EntradaServo, OUTPUT);
+  servo.attach(EntradaServo); // Vincular el servo al pin 6
+  servo.write(0); // Se situa el ángulo de este en 0 grados
+
+  Wire.begin();
+
+  ETin.begin(details(informacion), &Serial);
+}
+
+void loop() 
+{
+  valorHigrometro = analogRead(HigrometroPin); // Lectura Higrometro
+  valorHigrometroMap = map(valorHigrometro, 0, 1023, 100, 0); // Mapear resultado Higrometro
+
+  valorFlexometro = analogRead(FlexometroPin); // Lectura Flexometro
+  valorFlexometroMap = map(valorFlexometro, 32, 85, 1, 10); // Mapear resultado Flexometro
+
+  if(valorFlexometroMap <= 3) estadoTallo = "+Torcido";
+  else if (valorFlexometroMap >= 4 && valorFlexometroMap <= 7) estadoTallo = "Torcido";
+  else estadoTallo = "Recto";
+    
+   
+  valorHumedad = dht.readHumidity(); // Leer la humedad relativa
+  valorTemperatura = dht.readTemperature(); // Leer la temperatura, por defecto en grados centígrados
+   
+  if (isnan(valorHumedad) || isnan(valorTemperatura)) // Comprobar si ha habido un error en la obtención datos
+  {
+    Serial.println("ERROR DHT11");
+    return;
+  }
+
+  DateTime fechaActual = rtc3231.now(); // Obtener fecha actual
+  
+  fechaYhoraActual = horaYFechaActualString(fechaActual); // Convertir fecha y hora a String EJ 14:56 3/5
+
+  static int i = 0;
+  if (gpsSerial.available())
+  {
+    char ch = gpsSerial.read();
+    if (ch != '\n' && i < sentenceSize)
+    {
+      sentence[i] = ch;
+      i++;
+    }
+    else
+    {
+     sentence[i] = '\0';
+     i = 0;
+     displayGPS();
+    }
+  }
+
+  if(valorHigrometroMap < 50 || valorFlexometroMap <= 5) // La planta necesita agua, se riega con el motor
+  {
+    for(int i = 1; i <= cantidadRiego; i++) // Bucle para ejecutar el movimiento del servo 5 veces
+    {
+      servo.write(30);
+      delay(500);// Incrementar en 30 grados el eje del servo
+      servo.write(60);
+      delay(500);// Incrementar en 60 grados el eje del servo
+      servo.write(90);
+      delay(500);// Incrementar en 90 grados el eje del servo
+      servo.write(120);
+      delay(500);// Incrementar en 120 grados el eje del servo
+      servo.write(150);
+      delay(500);// Incrementar en 150 grados el eje del servo
+      servo.write(180);
+      delay(1000);// Incrementar en 180 grados el eje del servo
+      servo.write(0); // El servo vuelve a su estado normal
+      delay(500);
+    }
+  }
+  
+  fechaYhoraActual.toCharArray(arrayHoraYFecha,12);
+  localizacion.toCharArray(arrayLocalizacion, 23);
+  estadoTallo.toCharArray(arrayEstadoTallo, 9);
+  
+  informacion.higrometro = valorHigrometroMap;
+  informacion.humedad = valorHumedad;
+  informacion.temperatura = valorTemperatura;
+  memcpy(informacion.fechaYhora, arrayHoraYFecha, strlen(arrayHoraYFecha)+1);
+  memcpy(informacion.ubicacion, arrayLocalizacion, strlen(arrayLocalizacion)+1);
+  memcpy(informacion.estadoTallo, arrayEstadoTallo, strlen(arrayEstadoTallo)+1);
+
+  ETin.sendData();
+ 
+ delay(3000);
+  
+}
+
+void displayGPS()
+{
+  char field_1[20];
+  char field_2[20];
+  char letra_1[20];
+  char letra_2[20];
+  char numero_1[20];
+  char numero_2[20];
+  char salida[80];
+  getField(field_1, 0);
+  if (strcmp(field_1, "$GPRMC") == 0)
+  {
+    //convertimos el primer numero
+    getField(field_1, 3);  // number
+    number(numero_1,field_1);
+    //primera letra
+    getField(letra_1, 4); // N/S
+   //convertimos el segundo numero
+    getField(field_2, 5);  // number
+    number(numero_2,field_2);
+    //segunda letra
+    getField(letra_2, 6);  // E/W
+
+    String letra_1String(letra_1);
+    String numero_1String(numero_1);
+    String letra_2String(letra_2);
+    String numero_2StringString(numero_2);
+
+    localizacion = letra_1String + " " + numero_1String + " " + letra_2String + " " + numero_2StringString;
+  }
+}
+
+void number(char* numeroViejo,char* numeroNuevo)
+{
+  char aux;
+  int sentencePos = 0;
+  //con el while acortamos el numero de decimales ya que solo queremos 2
+  while (numeroViejo [sentencePos]!='.')
+  {
+      numeroNuevo[sentencePos]=numeroViejo[sentencePos];
+      sentencePos ++;
+  }
+  //secuencia para mover las comas
+   aux=numeroNuevo[sentencePos-1];
+   numeroNuevo[sentencePos-1]=numeroNuevo[sentencePos-2];
+   numeroNuevo[sentencePos-2]=',';
+   numeroNuevo[sentencePos]=aux;
+   
+   
+   // buffer[sentencePos+1] = '\0';
+  
+} 
+
+
+
+
+
+void getField(char* buffer, int index)
+{
+  int sentencePos = 0;
+  int fieldPos = 0;
+  int commaCount = 0;
+  while (sentencePos < sentenceSize)
+  {
+    if (sentence[sentencePos] == ',')
+    {
+      commaCount ++;
+      sentencePos ++;
+    }
+    if (commaCount == index)
+    {
+      buffer[fieldPos] = sentence[sentencePos];
+      fieldPos ++;
+    }
+    sentencePos ++;
+  }
+  buffer[fieldPos] = '\0';
+} 
+
+String horaYFechaActualString(DateTime fechaActual)
+{
+  String datos;
+
+  datos = fechaActual.hour();
+  datos = datos + ':';
+  datos = datos + fechaActual.minute();
+  datos = datos + ' ';
+  datos = datos + fechaActual.day();
+  datos = datos + '/';
+  datos = datos + fechaActual.month();
+
+  return datos;
+}
